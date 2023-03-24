0a1d142262cbdc8e43a2162e8d6f515787555cc5
azarnuzy <azarnuzy@gmail.com>
2023-03-25 04:05:15 +0700

    make responsive navbar & header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import { Inter } from '@next/font/google'
+import LandingPage from './LandingPage'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={inter.className}>
-      
+      <LandingPage />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('../../public/images/header-1.jpg')",
+      },
+      colors: {
+        'light-dark': '#222',
+        'light-red': '#fc5252',
+        'light-purple': '#381ddb',
+        'very-light-purple': '#bbb0fb',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 }
