9abd544580e222865c7979fb9756a18c5adcc91a
azarnuzy <azarnuzy@gmail.com>
2023-05-14 16:04:33 +0700

    add success submit component

---
+++ src/pages/daftar-ppdb/success.jsx
@@ -0,0 +1,12 @@
+import SuccessSubmit from '@/components/FormPPDB/SuccessSubmit'
+import React from 'react'
+
+function Success() {
+  return (
+    <>
+      <SuccessSubmit />
+    </>
+  )
+}
+
+export default Success
