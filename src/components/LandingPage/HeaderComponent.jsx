5073d26f8a0503dd81e019733e08b9b773d30066
axel eldrian hadiwibowo <eldrianaxel1@gmail.com>
2023-06-05 15:12:39 +0700

    change lorem to real text

--- src/components/LandingPage/HeaderComponent.jsx
+++ src/components/LandingPage/HeaderComponent.jsx
@@ -10,14 +10,6 @@ function HeaderComponent() {
         <h1 className='uppercase flex flex-col mb-6 text-3xl sm:text-5xl font-extrabold text-white text-center tracking-wider gap-3'>
           selamat datang di <span className='text-light-red'>sdn ciwaregu</span>
         </h1>
-        <div className='max-w-lg text-center text-white'>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Recusandae, vel! Deleniti esse, molestiae dolores quam sunt magni!
-            Rem, iste a? Lorem ipsum dolor sit, amet consectetur adipisicing
-            elit. Aliquid, officiis.
-          </p>
-        </div>
       </div>
     </div>
   )
